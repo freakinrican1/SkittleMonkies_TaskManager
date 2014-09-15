7bf8a0b309801e520f7898e9b7c2bcaf39457942
Nathaniel Ruiz <Nathruiz13@gmail.com>
2014-09-15 11:55:02 -0500

    Skittle Monkies launchpad

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TaskManager::Application.config.secret_token = '1f9f3af921459374e076a5f6218a0084ddf1bd322f93863cbf8acd5f7d61bf400c40e30cce70f71f50f27ed126da8aa013d5485ec1cdfe54f45372bc867810e8'
