0b806b31bca03c14154285bc64176e6631d7ad27
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-09-16 11:19:50 -0500

    added join table: projects_tasks

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   attr_accessible :description, :title, :username, :user_id
-  has_many :tasks # through => categories
+  has_many :tasks 
   belongs_to :user
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140916031420) do
+ActiveRecord::Schema.define(:version => 20140916161616) do
 
   create_table "projects", :force => true do |t|
     t.string   "title"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(:version => 20140916031420) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "user_id"
+  end
+
+  create_table "projects_tasks", :force => true do |t|
+    t.integer "project_id"
+    t.integer "task_id"
   end
 
   create_table "tasks", :force => true do |t|
