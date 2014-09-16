7bf0f7e9d6ad9d9733a3c352297ffcbcca9638f6
Matt Taylor <Matt@Navgear.com>
2014-09-15 20:07:53 -0500

    Created Category model and related table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140915214646) do
+ActiveRecord::Schema.define(:version => 20140916010504) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "tasks", :force => true do |t|
     t.string   "task_name"
