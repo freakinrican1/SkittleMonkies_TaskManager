ee69c844220634b9f57b84748c24791f2705aae0
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-09-15 21:40:07 -0500

    added project model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140915214646) do
+ActiveRecord::Schema.define(:version => 20140916023923) do
+
+  create_table "projects", :force => true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "username"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "tasks", :force => true do |t|
     t.string   "task_name"
