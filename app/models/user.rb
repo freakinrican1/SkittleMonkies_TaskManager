79829da462cff6f917998e4d8a4a4dba1895eba8
Nathaniel Ruiz <Nathruiz13@gmail.com>
2014-09-15 22:45:05 -0500

    Validations and flash alerts (to be styled)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
   attr_accessible :username, :email, :password, :password_confirmation
   has_many :tasks
+  
+   validates :username, presence: true, uniqueness: true
+    validates :email,  presence: true, uniqueness: true
+    validates :password, presence: true , confirmation: true
+    validates :password_confirmation, presence: true
+
 
   has_secure_password
 
